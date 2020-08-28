c9e0dca88c6a50ecc9e5e94c5a75dfcbed8e7711
Stas <example@gmail.com>
2020-08-28 16:36:57 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 "use strict";
 
-{
-    var gg = 59;
-}
-    console.log(gg);  
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов', ''),
+      b = prompt('Оценка?', ''),
+      c = prompt('Один из последних фильмов', ''),
+      d = prompt('Оценка?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
